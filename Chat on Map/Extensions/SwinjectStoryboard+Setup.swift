2dcc26fb2ef27c1a7af37b66c286ad1f5bb1f35c
Fabio Hiroki <fabiothiroki@gmail.com>
2017-07-23 19:10:31 -0300

    Test Magnetic dependency

--- Chat on Map/Extensions/SwinjectStoryboard+Setup.swift
+++ Chat on Map/Extensions/SwinjectStoryboard+Setup.swift
@@ -13,12 +13,17 @@ import CoreLocation
 extension SwinjectStoryboard {
     class func setup() {
         defaultContainer.storyboardInitCompleted(ViewController.self) { r, c in
-            c.userLocationDataSource = r.resolve(UserLocationResource.self)
+            c.userLocationResource = r.resolve(UserLocationResource.self)
+            c.peopleLocationResource = r.resolve(PeopleLocationResourceProtocol.self)
         }
         
         defaultContainer.register(LocationManager.self) { _ in CLLocationManager() }
         defaultContainer.register(UserLocationResource.self) { r in
             UserLocationResource.init(locationManager: r.resolve(LocationManager.self)!)
+        }
+        
+        defaultContainer.register(PeopleLocationResourceProtocol.self) {
+            _ in PeopleLocationResource()
         }
 
     }
--- Chat on Map/ViewController.swift
+++ Chat on Map/ViewController.swift
@@ -9,17 +9,90 @@
 import UIKit
 import CoreLocation
 import MapKit
+import SpriteKit
+import Magnetic
+
 
 class ViewController: UIViewController {
-
-    @IBOutlet fileprivate var mapView: MKMapView!
     
-    var userLocationDataSource: UserLocationResource?
+    var userLocationResource: UserLocationResource?
+    var peopleLocationResource: PeopleLocationResourceProtocol?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(userLocationDataSource)
+        let magneticView = MagneticView.init(frame: self.view.bounds)
+        
+        self.view.addSubview(magneticView)
+        
+        for _ in 0..<12 {
+            add(magneticView.magnetic)
+        }
     }
+    
+    @IBAction func add(_ magnetic: Magnetic) {
+        let name = UIImage.names.randomItem()
+        let color = UIColor.colors.randomItem()
+        let node = Node(text: name.capitalized, image: UIImage(named: name), color: color, radius: 40)
+        magnetic.addChild(node)
+    }
+}
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        self.init(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
+    }
+    
+    static var red: UIColor {
+        return UIColor(red: 255, green: 59, blue: 48)
+    }
+    
+    static var orange: UIColor {
+        return UIColor(red: 255, green: 149, blue: 0)
+    }
+    
+    static var yellow: UIColor {
+        return UIColor(red: 255, green: 204, blue: 0)
+    }
+    
+    static var green: UIColor {
+        return UIColor(red: 76, green: 217, blue: 100)
+    }
+    
+    static var tealBlue: UIColor {
+        return UIColor(red: 90, green: 200, blue: 250)
+    }
+    
+    static var blue: UIColor {
+        return UIColor(red: 0, green: 122, blue: 255)
+    }
+    
+    static var purple: UIColor {
+        return UIColor(red: 88, green: 86, blue: 214)
+    }
+    
+    static var pink: UIColor {
+        return UIColor(red: 255, green: 45, blue: 85)
+    }
+    
+    static let colors: [UIColor] = [.red, .orange, .yellow, .green, .tealBlue, .blue, .purple, .pink]
+    
+}
+
+extension UIImage {
+    
+    static let names: [String] = ["argentina", "bolivia", "brazil", "chile", "costa rica", "cuba", "dominican republic", "ecuador", "el salvador", "haiti", "honduras", "mexico", "nicaragua", "panama", "paraguay", "peru", "venezuela"]
+    
+}
+
+extension Array {
+    
+    func randomItem() -> Element {
+        let index = Int(arc4random_uniform(UInt32(self.count)))
+        return self[index]
+    }
+    
 }
 
